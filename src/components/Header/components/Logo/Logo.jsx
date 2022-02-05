10e75f347ad8b491b023d31a517e0f47dd2b008c
Raviteja <Raviteja_Geddada@epam.com>
2022-02-05 15:40:37 +0530

    React Task 1 - Components, state and props

--- src/components/Header/components/Logo/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import logo from '../../../../assests/Logo.png';
-
-const Logo = () => <img src={logo} alt='logo' />;
-
-export default Logo;
--- src/helpers/pipeDuration.js
+++
@@ -1,9 +0,0 @@
-const convertMinsToHrsMins = (minutes) => {
-	let hour = Math.floor(minutes / 60);
-	let min = minutes % 60;
-	hour = hour < 10 ? '0' + hour : hour;
-	min = min < 10 ? '0' + min : min;
-	return `${hour}:${min}`;
-};
-
-module.exports = { convertMinsToHrsMins };
